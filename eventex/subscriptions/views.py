063adb5594b9cea7b1129150c5a4e0b4d9dcfb2a
Gabriel Zucoloto <gtzucoloto@gmail.com>
2020-11-12 14:44:46 -0300

    From alinhado com Sender Identity

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -16,8 +16,8 @@ def subscribe(request):
                 'subscriptions/subscription_email.txt', form.cleaned_data)
             mail.send_mail('Confirmação de inscrição',
                            body,
-                           'contato@eventex.com.br',
-                           ['contato@eventex.com.br', form.cleaned_data['email']])
+                           'gtzucoloto@gmail.com',
+                           ['gtzucoloto@gmail.com', form.cleaned_data['email']])
 
             messages.success(request, 'Inscrição realizada com sucesso!')
             return HttpResponseRedirect('/inscricao/')
